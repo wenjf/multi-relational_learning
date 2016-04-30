5f0f7e17d2355c52fbc48e8ad3e22bc011269037
wenjf <wenjif@buaa.edu.cn>
2016-04-30 10:49:12 +0800

    Revert "the first commit"
    This reverts commit 66cab583005ff7cc4fd87bcf090d2e1957b9ce78.

--- m-transH_ID/run_test_direct_with_raw_detail.sh
+++
@@ -1,8 +0,0 @@
-dims=(25 50)
-
-log_prefix=/storage1/kbData/enforce
-
-for dim in ${dims[@]}; do
-	nohup bash arma_test/test_direct_with_raw_detail.sh $dim 0.0015 1000 1 $log_prefix/detail.direct.with.raw.$dim > $log_prefix/log.direct.with.raw.$dim &
-	sleep 30
-done
